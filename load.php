4be3c72fec6092cb55882aa9744c549af5ac9341
Michael Wagstaff <michaelwagstaff2000@gmail.com>
2017-07-23 12:25:37 +0100

    Removed unneeeded files

--- load.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "../wikiHeader.php";
-require "utils.php";
-require "pageFetch.php";
-$utils = new utils;
-$pageFetch = new pageFetch;
-echo $pageFetch->constructor("");
-include "../wikiFooter.php";
-?>
